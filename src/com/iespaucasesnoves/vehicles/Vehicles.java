dede889db3e803ad262964a54ca67e4fa89e34df
RaulIntelliJ <martinezvjraul@gmail.com>
2019-04-29 17:52:54 +0200

    Clase vehiculos añadida

---
+++ src/com/iespaucasesnoves/vehicles/Vehicles.java
@@ -0,0 +1,8 @@
+package com.iespaucasesnoves.vehicles;
+
+public class Vehicles {
+    private String matricula;
+    private double precioHora;
+    private String tipoCarnet;
+    private TipoCoche tipoCoche;
+}
