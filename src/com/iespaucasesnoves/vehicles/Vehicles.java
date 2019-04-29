0cd2d1b6c1e0e325810e24d3b1e732f4e372e56d
RaulIntelliJ <martinezvjraul@gmail.com>
2019-04-29 18:18:50 +0200

    Completar el paquete vehiculo

--- src/com/iespaucasesnoves/vehicles/Vehicles.java
+++
@@ -1,8 +0,0 @@
-package com.iespaucasesnoves.vehicles;
-
-public class Vehicles {
-    private String matricula;
-    private double precioHora;
-    private String tipoCarnet;
-    private TipoCoche tipoCoche;
-}
